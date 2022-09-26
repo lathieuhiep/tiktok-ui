29f1b2e757118d095e9f708b5168eaf9c1620f5b
lakhacdiep <diep.lk@beetechsoft.com>
2022-09-26 10:57:36 +0700

    active menu

--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
+const handelActiveMenu = (nav) => {
+    console.log(nav)
+    return nav.isActive ? 'active' : ''
+}
+
 function MenuItem({ title, to, icon }) {
     return (
-        <NavLink to={to}>
+        <NavLink className={(nav) => handelActiveMenu(nav)} to={to}>
             {icon}
             <span>{title}</span>
         </NavLink>
